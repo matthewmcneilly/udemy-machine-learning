798af1b9de2e394edc978c628c7813fb72a55418
matthewmcneilly <matthewmcneilly182@gmail.com>
2017-08-18 19:16:33 +0100

    Encoding categorical data

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -6,12 +6,22 @@ dataset = read.csv('Data.csv')
 # Take care of missing values 
 # Age Columns
 dataset$Age = ifelse(is.na(dataset$Age), # If no value 
-  ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), # Use the mean average  
-  dataset$Age) # If there is a value use it 
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), # Use the mean average  
+                     dataset$Age) # If there is a value use it 
 # Salary Column 
 dataset$Salary = ifelse(is.na(dataset$Salary),
-  ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), 
-  dataset$Salary) 
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), 
+                        dataset$Salary) 
+
+# Encoding categorical data 
+# Country Column
+dataset$Country = factor(dataset$Country, 
+                         levels = c('France', 'Spain', 'Germany'), # Creates a factor with three elements 
+                         labels = c(1, 2, 3)) # Assign numbers to the categorical data 
+# Purchased Column 
+dataset$Purchased = factor(dataset$Purchased, 
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1)) 
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -18,8 +18,28 @@ imputer = imputer.fit(X[:, 1:3]) # Grab the second and third column
 X[:, 1:3] = imputer.transform(X[:, 1:3]) # Replace missing value with average 
 
 # Encoding categorical data 
-from sklearn.preprocessing import LabelEncoder 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder 
 labelencoder_X = LabelEncoder()
+""" 
+Encodes first column as numbers (France === 0)
+so that they can be used in mathematical equations
+"""
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0]) 
+"""
+Encode dummy variables
+ensuring equations dont attribute an order to the categorical variables 
+eg. France cant be greater than Germany, doesnt make any sense.  
+So it creates a column to represent each categorical variable using 1's and 0's
+as true or false
+"""  
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encode the last column 
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y) 
+
+
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
